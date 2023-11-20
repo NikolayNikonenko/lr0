03e1c789ee10560f593248ca6de7d9c36a6b11f5
kettler090@gmail.com <kettler090@gmail.com>
2023-11-20 18:24:00 +0700

    Добавьте файлы проекта.

---
+++ Задание 3.21 б/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите значение a:");
+double a = Convert.ToDouble(Console.ReadLine());
+double a2, a4, a6, a10;
+a2 = a * a;
+a4 = a2 * a2;
+a6 = a4 * a2;
+a10 = a4 * a6;
+Console.WriteLine($"a={a} в 10 степени = {a10}");
